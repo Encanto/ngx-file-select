3e3df9b8c51acb9d0c4902f877ad745d42b3f3f3
WilliamRClark <cwilliamwork@yahoo.com>
2021-05-26 15:10:32 -0400

    Fill in the rest of the data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,27 @@ export class AppComponent {
     )
   }
 
+  public getHumanSize(file) {
+    // Just return an empty size for folders
+    if (file.folder) {
+      return '';
+    }
+    let size = file.size;
+    var hz;
+    if (size < 1024) hz = size + ' B';
+    else if (size < 1024*1024) hz = (size/1024).toFixed(2) + ' KB';
+    else if (size < 1024*1024*1024) hz = (size/1024/1024).toFixed(2) + ' MB';
+    else hz = (size/1024/1024/1024).toFixed(2) + ' GB';
+    return hz;
+  }
+
+  public getHumanTime(file) {
+    var t = new Date(file.mtime);
+    return t.toLocaleDateString() + ' ' + t.toLocaleTimeString();
+  }
+
   public getCssClass(file) {
-    return file ? 'fa fa-folder-o fa-lg' : 'fa fa-file-o fa-lg';
+    return file ? 'fa fa-folder-open-o' : 'fa fa-file-o';
   }
 
   title = 'ngx-file-select';
